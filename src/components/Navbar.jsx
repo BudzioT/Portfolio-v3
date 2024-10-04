36bf6001ce9e9279667bdef3614c647448a26735
BudzioT <budzio.zsm1@gmail.com>
2024-10-04 10:32:58 +0200

    Started nav

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <>
+            <div className={"navbar navbar-static-top navbar-inverse"}>
+                <div className={"navbar-inner"}>
+                    <div className={"container"}>
+                        <a className={"brand"} href={"#"}>Bartosz</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
